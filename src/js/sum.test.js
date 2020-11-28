b0fe9418a7fb0e6120054911cbb97360b656889b
Рощупкин Виталий <elpistolero1999@gmail.com>
2020-11-28 10:54:26 +0300

    edit sum.test.js

--- src/js/sum.test.js
+++ src/js/sum.test.js
@@ -1,5 +1,10 @@
+const { exception } = require('console');
 const sum = require('./sum');
 
 test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(3);
+});
+
+test('adds 8 + 8 to equal 16', () => {
+    expect(sum(8, 8)).toBe(16);
 });
